c102be2fac70295cbc8364da317f8d744cf8298c
bkisly <b.kisly@gmail.com>
2022-11-28 14:48:09 +0100

    Created empty API project

---
+++ swizleApi/src/main/java/swizle/SwizleApiApplication.java
@@ -0,0 +1,14 @@
+package swizle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class SwizleApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwizleApiApplication.class, args);
+    }
+
+}
---
+++ swizleApi/src/test/java/swizle/SwizleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package swizle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwizleApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
