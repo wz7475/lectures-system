8a216acf0db0fcefdb9a517bb70eb88935a6b6bc
bkisly <b.kisly@gmail.com>
2023-01-16 18:57:23 +0100

    Completed the algoritm for automatic lectures swapping

--- swizleApi/src/main/java/swizle/utils/RoutineManager.java
+++ swizleApi/src/main/java/swizle/utils/RoutineManager.java
@@ -1,28 +1,32 @@
 package swizle.utils;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import swizle.models.Offer;
-import swizle.services.OfferDataService;
+import swizle.services.interfaces.ILectureDataService;
 import swizle.services.interfaces.IOfferDataService;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
+@Transactional
 public class RoutineManager {
     private final IOfferDataService offerDataService;
+    private final ILectureDataService lectureDataService;
 
     @Autowired
-    public RoutineManager(@Qualifier(Constants.OfferServiceQualifier) IOfferDataService offerDataService) {
+    public RoutineManager(@Qualifier(Constants.OfferServiceQualifier) IOfferDataService offerDataService,
+                          @Qualifier(Constants.LectureServiceQualifier) ILectureDataService lectureDataService) {
         this.offerDataService = offerDataService;
+        this.lectureDataService = lectureDataService;
     }
 
     @Scheduled(cron = "*/10 * * * * *")
     public void reportTask() {
-        System.out.println("Automatically resolving complementary offers...");
         List<Long> resolvedOffers = new ArrayList<>();
 
         for(Offer offer1 : offerDataService.getItems()) {
@@ -34,14 +38,26 @@ public class RoutineManager {
 
                 if(offer1.getOfferedLectureId() == offer2.getReturnedLectureId()
                 || offer1.getReturnedLectureId() == offer2.getOfferedLectureId()) {
-                    System.out.print("Swapping lectures assignments for offers of id ");
-                    System.out.print(offer1.getId());
-                    System.out.print(" and ");
-                    System.out.println(offer2.getId());
+                    String swapLog = "Swapping lectures assignments for offers of id " +
+                            offer1.getId() +
+                            " and " +
+                            offer2.getId();
+
+                    System.out.println(swapLog);
+
+                    lectureDataService.signUpForLecture(offer1.getOfferedLectureId(), offer2.getSellerId());
+                    lectureDataService.signUpForLecture(offer2.getOfferedLectureId(), offer1.getSellerId());
+
+                    lectureDataService.optOutOfLecture(offer1.getOfferedLectureId(), offer1.getSellerId());
+                    lectureDataService.optOutOfLecture(offer2.getOfferedLectureId(), offer2.getSellerId());
+
                     resolvedOffers.add(offer1.getId());
                     resolvedOffers.add(offer2.getId());
                 }
             }
         }
+
+        for(long offerId : resolvedOffers)
+            offerDataService.deleteItem(offerId);
     }
 }
